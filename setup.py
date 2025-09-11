ca04aff8fdfa5352ebdf77283ca27bd30536d48d
Nriver <6752679+Nriver@users.noreply.github.com>
2025-09-11 11:48:22 +0800

    update to 1.2.3

--- setup.py
+++ setup.py
@@ -118,6 +118,7 @@ setup(
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
         'Programming Language :: Python :: 3 :: Only',
     ],
     # This field adds keywords for your project which will appear on the
